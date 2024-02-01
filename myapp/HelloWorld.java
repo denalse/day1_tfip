6e87603d3eea6748cbf90aee60eac65e4f5f5fbe
denalse <denaleewonders@gmail.com>
2024-02-01 12:40:32 +0800

    test

---
+++ myapp/HelloWorld.java
@@ -0,0 +1,18 @@
+package myapp;
+
+//import java.io.Console;
+import java.lang.*;
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello I am tired...");
+        
+        // Console console = System.console();
+
+        // String name = console.readLine("What is your name?");
+    
+        // System.out.println("Hello " + name + ", Please to meet you.");
+        // System.out.printf("Hello %s + , Please to meet you.", name);
+    }
+}
